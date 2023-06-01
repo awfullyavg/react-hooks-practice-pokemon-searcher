075c2b394d9c8909ec3243320c40f0475e8c378f
Bryant Wiggins <topwigit@gmail.com>
2023-05-31 21:16:15 -0400

    practice

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,22 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon}) {
+  const mappedPokemon = pokemon.map((pokemon) => (
+    <PokemonCard 
+    key={pokemon.id}
+    name={pokemon.name}
+    hp={pokemon.hp}
+    front={pokemon.sprites.front}
+    back={pokemon.sprites.back}
+    />
+  ))
+
+  
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {mappedPokemon}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+    .then((resp) => resp.json())
+    .then((data) => setPokemon(data))
+  }, [])
+
+  const [pokemon, setPokemon] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const filteredPokemon = pokemon.filter((pokemon) => (
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ))
+
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm pokemon={pokemon} setPokemon={setPokemon}/>
       <br />
-      <Search />
+      <Search setSearchTerm={setSearchTerm}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={filteredPokemon}/>
     </Container>
   );
 }
